bbf54089d8250edd3faa5f893a4a1c0988626f3d
ReetBarik <reetbarik94@gmail.com>
2020-09-05 14:23:37 -0700

    Camera Ready changes to Perf Plots

--- IISWC2020/SmallExperiments/PerfPlot/relative_perfs.py
+++ IISWC2020/SmallExperiments/PerfPlot/relative_perfs.py
@@ -28,12 +28,12 @@ def draw_lines(input_name):
     sns.set_style("whitegrid", {'grid.color': '.5', 'grid.linestyle': u'-'})
     # palette = sns.cubehelix_palette(n_colors=3, start=0, rot=0.17, gamma=3.1, \
     #                             hue=0.9, light=0.9, dark=0.5, reverse = False, as_cmap=False)
-    # colors = ["#3498db", "#e74c3c", "#2ecc71", "#000000", "#3498db", "#e74c3c", "#2ecc71", "#3498db", "#e74c3c", "#2ecc71", "#3498db"]
-    # palette = sns.color_palette(colors)
-    palette = sns.hls_palette(4)
+    colors = ["#3498db", "#e74c3c", "#2ecc71", "#000000", "#3498db", "#e74c3c", "#2ecc71", "#3498db", "#e74c3c", "#2ecc71", "#3498db"]
+    palette = sns.color_palette(colors)
+    # palette = sns.hls_palette(6)
     sns.set_palette(palette)
 
-    fig = plt.figure(figsize = (30, 15), dpi = 150)
+    fig = plt.figure(figsize = (40, 17), dpi = 150)
     #fig = plt.figure(dpi=150)
     ax = plt.gca()
     fig.canvas.draw()
@@ -72,19 +72,19 @@ def draw_lines(input_name):
     # associate each tick with thread number
     #ax.xaxis.grid(False)
     ax.tick_params(labelsize = 45)
-    # ax.set_xlabel('Relative Performance of Different Reordering Schemes', fontsize = font_size)
+    ax.set_xlabel('Performance Relative to the Best Algorithm', fontsize = font_size, labelpad=30)
     ax.set_ylabel('Fraction of datasets', fontsize = font_size)
     # associate each tick with thread number
     ax.set_ylim([0, 1.05])
     ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
     ax.set_yticklabels(['0%', '25%', '50%', '75%', '100%'])
     
-    schemes = ['Natural', 'Metis', 'DegSort', 'RCM', 'Gorder', 'Grappolo', 'Random', 'Grappolo-RCM', 'Rabbit', 'Slashburn', 'ND']
+    schemes = ['Natural', 'METIS', 'Degree Sort', 'RCM', 'Gorder', 'Grappolo', 'Random', 'Grappolo-RCM', 'Rabbit-Order', 'SlashBurn', 'ND']
     # set legend, exclude threadnumber
-    leg = fig.legend(ax.lines, list(df), \
+    leg = fig.legend(ax.lines, schemes, \
                     ncol = 1, frameon=True, fancybox = True, \
                     prop={'size':45}, shadow = False, framealpha=1.0, \
-                    bbox_to_anchor = (0.77, 0.57)) #'xx-large')
+                    bbox_to_anchor = (0.79, 0.75)) #'xx-large')
     leg.get_frame().set_edgecolor('k')
     leg.get_frame().set_linewidth(1)	
     #plt.title(input_name + ' Arrangement', {'fontsize': 50})
@@ -96,11 +96,11 @@ def draw_lines(input_name):
     return;
 
 # change path accordingly
-# draw_lines('Average Linear')
+draw_lines('Average Linear')
 # draw_lines('Maximum Linear')
 # draw_lines('Bandwidth Avg')
 # draw_lines('Time')
 # draw_lines('Jugaad')
 # draw_lines('Metis')
 # draw_lines('SingleThread')
-draw_lines('SingleThreadIntel')
+# draw_lines('SingleThreadIntel')
